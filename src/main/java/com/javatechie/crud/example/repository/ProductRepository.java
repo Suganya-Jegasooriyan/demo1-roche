759efb45caff0e91010e677168a5e14ad3725f2b
Suganya-Jegasooriyan <suganya.jegasooriyan@ideas2it.com>
2023-06-20 11:29:05 +0530

    test

--- src/main/java/com/javatechie/crud/example/repository/ProductRepository.java
+++ src/main/java/com/javatechie/crud/example/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import com.javatechie.crud.example.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product,Integer> {
+    //test commit
     Product findByName(String name);
 }
 
